453938721b50da28ae0f3c947e2b5a6c14acf08a
r.almeida.barbosa <r.almeida.barbosa@CPX-GK35ZD7QU6O.dir.svc.accenture.com>
2017-10-18 15:36:05 -0200

    ADO02

---
+++ src/ADO02/filaClass.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ADO02;
+
+/**
+ *
+ * @author r.almeida.barbosa
+ */
+public class filaClass {
+    public Object [] fila; 
+    public int inicio;
+    public int fim;
+    
+    public filaClass(){
+        this.inicio = -1;
+        this.fim = -1;
+        this.fila = new Object [5];
+    }
+    
+    public boolean isEmpty(){
+        if ((this.inicio == -1)&&(this.fim == -1)){
+            return true;
+        }
+        return false;
+    }
+    
+    public int size(){
+        if (this.isEmpty()){
+            return 0;
+        }
+        return this.fila.length;
+    }
+    
+    public Object inserirEnqueue(Object valor){
+        if (this.fim <= this.fila.length -1){
+            this.fila[++fim] = valor;
+            if (inicio == -1){
+                inicio += 1;
+            }
+        }return valor;
+    }
+    
+    public Object removerDequeue(){
+        if (!isEmpty()){
+            Object x = this.fila[inicio];
+            inicio += 1;
+            return x;
+        }
+        return null;
+    }
+    
+    public static void main(String[] args) {
+        filaClass fila = new filaClass();
+        
+        fila.removerDequeue();
+    }
+}
