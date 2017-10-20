c6fc883369984ab8e33a35b94f402e3dbcec9692
r.almeida.barbosa <r.almeida.barbosa@CPX-GK35ZD7QU6O.dir.svc.accenture.com>
2017-10-20 15:29:30 -0200

    ADO02

--- src/ADO02/Deque.java
+++ src/ADO02/Deque.java
@@ -5,106 +5,31 @@
  */
 package ADO02;
 
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Queue;
+import java.util.ArrayDeque;
+import java.util.LinkedList;
+import sun.misc.Queue;
 
 /**
  *
  * @author r.almeida.barbosa
  */
-public class Deque {
+public class Deque extends Queue {
 
-    Queue deque = new Queue() {
-        @Override
-        public boolean add(Object e) {
-            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-        }
+    public static void main(String[] args) {
+        LinkedList dequeA = new LinkedList();
+        ArrayDeque dequeB = new ArrayDeque();
+        Deque dequeC = new Deque();
+        
+        //dequeA.add
 
-        @Override
-        public boolean offer(Object e) {
-            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-        }
+        //dequeA.addFirst(10);
+        //dequeA.addFirst(20);
+        dequeA.addFirst(30);
+        dequeA.addLast(5);
+               
+        System.out.println("deque A: " + dequeA);
+        System.out.println("deque B: " + dequeB);
+        System.out.println("deque C: " + dequeC);
+    }
 
-        @Override
-        public Object remove() {
-            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-        }
-
-        @Override
-        public Object poll() {
-            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-        }
-
-        @Override
-        public Object element() {
-            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-        }
-
-        @Override
-        public Object peek() {
-            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-        }
-
-        @Override
-        public int size() {
-            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-        }
-
-        @Override
-        public boolean isEmpty() {
-            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-        }
-
-        @Override
-        public boolean contains(Object o) {
-            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-        }
-
-        @Override
-        public Iterator iterator() {
-            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-        }
-
-        @Override
-        public Object[] toArray() {
-            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-        }
-
-        @Override
-        public Object[] toArray(Object[] ts) {
-            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-        }
-
-        @Override
-        public boolean remove(Object o) {
-            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-        }
-
-        @Override
-        public boolean containsAll(Collection clctn) {
-            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-        }
-
-        @Override
-        public boolean addAll(Collection clctn) {
-            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-        }
-
-        @Override
-        public boolean removeAll(Collection clctn) {
-            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-        }
-
-        @Override
-        public boolean retainAll(Collection clctn) {
-            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-        }
-
-        @Override
-        public void clear() {
-            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-        }
-    };
 }
